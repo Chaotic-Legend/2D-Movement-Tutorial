5caedd5400c2eb7d38c1bf74b3e2c3155c70c4fe
Chaotic-Legend <gamemasterx40@gmail.com>
2026-02-12 13:04:52 -0500

    Added collision, a game camera, and dashing variables.

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -2,14 +2,24 @@ extends CharacterBody2D
 
 @export var walk_speed = 150.0
 @export var run_speed = 250.0
-@export_range(0, 1) var accerleration = 0.1
+@export_range(0, 1) var acceleration = 0.1
 @export_range(0, 1) var deceleration = 0.1
 
 @export var jump_force = -400.0
 @export_range(0, 1) var decelerate_on_jump_release = 0.5
 
-# Get the gravity from the project settings to be synched with RigidBody nodes.
+@export var dash_speed = 1000.0
+@export var dash_max_distance = 300.0
+@export var dash_curve : Curve
+@export var dash_cooldown = 1.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+var is_dashing = false
+var dash_start_position = 0
+var dash_direction = 0
+var dash_timer = 0
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -17,7 +27,7 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and (is_on_floor() or is_on_wall()):
 		velocity.y = jump_force
 	
 	if Input.is_action_just_released("jump") and velocity.y < 0:
@@ -33,7 +43,7 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
 	if direction:
-		velocity.x = move_toward(velocity.x, direction * speed, speed * accerleration)
+		velocity.x = move_toward(velocity.x, direction * speed, speed * acceleration)
 	else:
 		velocity.x = move_toward(velocity.x, 0, walk_speed * deceleration)
 
