199682ab6d004b087292ada1889c7c77f86501be
Chaotic-Legend <gamemasterx40@gmail.com>
2026-02-11 20:13:42 -0500

    Updated the script to add variables for walking, running, and jumping.

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -1,23 +1,40 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var walk_speed = 150.0
+@export var run_speed = 250.0
+@export_range(0, 1) var accerleration = 0.1
+@export_range(0, 1) var deceleration = 0.1
 
-func _physics_process(delta: float) -> void:
+@export var jump_force = -400.0
+@export_range(0, 1) var decelerate_on_jump_release = 0.5
+
+# Get the gravity from the project settings to be synched with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += gravity * delta
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_force
+	
+	if Input.is_action_just_released("jump") and velocity.y < 0:
+		velocity.y *= decelerate_on_jump_release
+	
+	var speed
+	if Input.is_action_pressed("run"):
+		speed = run_speed
+	else:
+		speed = walk_speed
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x, direction * speed, speed * accerleration)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, walk_speed * deceleration)
 
 	move_and_slide()
